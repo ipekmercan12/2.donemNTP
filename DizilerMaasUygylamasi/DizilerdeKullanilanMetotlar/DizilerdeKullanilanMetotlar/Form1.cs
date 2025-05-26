b6991bfdc8e3ce75c52e4a10484166d876dac17c
ipekmercan12 <ipekmercan0824@gmail.com>
2025-05-26 23:03:55 +0300

    Add files via upload

---
+++ DizilerMaasUygylamasi/DizilerdeKullanilanMetotlar/DizilerdeKullanilanMetotlar/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DizilerdeKullanilanMetotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("Dizinin eleman sayısı:" + numbers.Length  );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[,] matrix = {
+                {1,2 },
+                {3,4 }
+            };
+            MessageBox.Show("dizinin boyut sayısı (Rank) :" +matrix.Rank);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizinin en büyük elemanı : " + numbers.Max());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizilerin en küçük elemanı :" + numbers.Min());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizinin elemanlarının toplamı:" + numbers.Sum());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizinin elamanlarının ortalaması:"+numbers. Average());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizinin ilk elemanı :" + numbers. First());
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            MessageBox.Show("dizinin son elemanı: " + numbers.Last ());
+        }
+    }
+}
---
+++ hashTableUygulama/hashTableUygulama/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+class program
+{
+    static void Main(string[]args)
+    {
+        Hashtable ogrenciler = new Hashtable();
+        // veri ekleme
+        ogrenciler.Add(43, "damla");
+        ogrenciler.Add("rüzgar",147);
+        ogrenciler.Add("ipek", 203);
+        ogrenciler.Add("burak", 45);
+        ogrenciler.Add("ebrar"),1002;
+        // silme
+        ogrenciler.Remove(43);
+        ogrenciler.Remove("burak");
+        //güncelleme
+        ogrenciler[147] = "emirhan";
+        ogrenciler["ipek"] = 205;
+
+        foreach (var anahtar in ogrenciler.Values)
+        {
+            Console.WriteLine(anahtar);
+        }
+
+        foreach (DictionaryEntry eleman in ogrenciler)
+        {
+            Console.WriteLine(eleman.Key + "-" + eleman.Value);
+
+        }
+        Console.ReadLine();
+    }
+}
